19d2143403801e67bc8af8fe5e90e2e1477e169c
AnastasiaCris <cristeaanastasia13@gmail.com>
2022-12-07 15:10:00 +0100

    no comment

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -1,7 +1,8 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+
 [CreateAssetMenu(fileName = "Item", menuName = "Items/ItemType")]
 
 public class Item : ScriptableObject
@@ -9,7 +10,11 @@ public class Item : ScriptableObject
     public enum Ingredient
     {
         None,
-        Nuts
+        Water = 1 << 0,
+        Nut = 1 << 1,
+        Milk = 1 << 2,
+        Egg = 1 << 3,
+        All = ~0
     }
 
     public enum ItemType
@@ -27,16 +32,52 @@ public class Item : ScriptableObject
 
     public Sprite itemSprite;
     public ItemType itemType;
-    public List<Ingredient> ingredients;
+    [HideInInspector]
+    public Ingredient ingredients;
     
-    [Header("Cure Symptoms")]
+    //if it's a remedy
+    [HideInInspector]
     public bool isRemedy;//if ticked yes then show next
-    public List<RemedyType> remedyTypesAndTheirSuccessRate;//how much is it gonna help with each symptom
-}
+    [HideInInspector]
+    public Symptoms.Reaction[] reactionType = new Symptoms.Reaction[5];//how much is it gonna help with each symptom
+    [HideInInspector]
+    public float[] successRate = new float[5];//how much is it gonna help with each symptom
+    
+    //if it's an allergen
+    [Space]
+    public bool isAllergen;
+    
+    #region Editor
+#if UNITY_EDITOR
+    [CustomEditor(typeof(Item))]
+    public class ItemEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            
+            Item script = (Item)target;
 
-[Serializable]
-public class RemedyType 
-{
-    public Symptoms.Reaction ReactionType;
-    public float successRate;
+            script.ingredients = (Ingredient)EditorGUILayout.EnumFlagsField("Ingredients",script.ingredients);
+            
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Cure Symptoms");
+            // draw checkbox for the bool
+            script.isRemedy = EditorGUILayout.Toggle("Is Remedy", script.isRemedy);
+            
+            
+            if (script.isRemedy) // if bool is true, show other fields
+            {
+                for (int i = 0; i < script.reactionType.Length; i++)
+                {
+                    EditorGUILayout.BeginHorizontal();
+                    script.reactionType[i] = (Symptoms.Reaction)EditorGUILayout.EnumPopup(script.reactionType[i], GUILayout.MaxWidth(100));
+                    script.successRate[i] = EditorGUILayout.Slider("Success rate",script.successRate[i], 0, 100);
+                    EditorGUILayout.EndHorizontal();
+                }
+            }
+        }
+    }
+#endif
+    #endregion
 }
