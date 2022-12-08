f71cc2535827e72e2dc3a43041e016238304f615
AnastasiaCris <cristeaanastasia13@gmail.com>
2022-12-08 13:08:04 +0100

    You can choose pets with random variables now

--- Assets/Scripts/PetStats/PetStatsText.cs
+++ Assets/Scripts/PetStats/PetStatsText.cs
@@ -3,21 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PetStatsText : MonoBehaviour
 {
     [SerializeField]private PetStatsRandomizer statsRandomizer;
     public TextMeshProUGUI[] petInfoText;
+    public Slider[] statSliders;
     //For Showing Chosen Pets
     public ChosenPets chosenPetsScript;
     public TextMeshProUGUI[] petsChosenText;
-
+    public GameObject[] uiObj;//until 2 it's panel info about pets
+    public Image[] chosenPetsImg;
     public int currentSelectedPet;
     private void Start()
     {
-        for (int i = 0; i < petsChosenText.Length; i++)
+        for (int i = 0; i < chosenPetsScript.maxPets; i++)
         {
             petsChosenText[i].text = "No pet chosen";
+            chosenPetsImg[i].color = Color.white;//show how many available slots your have
         }
     }
 
@@ -25,6 +29,7 @@ public class PetStatsText : MonoBehaviour
     {
         for (int i = 0; i < chosenPetsScript.petsChosen.Count; i++)
         {
+            chosenPetsImg[i].GetComponent<Button>().enabled = true;
             petsChosenText[i].text = chosenPetsScript.petsChosen[i].petID + " chosen";
         }
     }
@@ -40,7 +45,15 @@ public class PetStatsText : MonoBehaviour
         {
             chosenPetsScript.petsChosen.Remove(chosenPetsScript.petsChosen[currentSelectedPet]);
             petsChosenText[currentSelectedPet].text = "No pet chosen";
+            petsChosenText[chosenPetsScript.petsChosen.Count].text = "No pet chosen";
+            chosenPetsImg[currentSelectedPet].GetComponent<Button>().enabled = false;
+            chosenPetsImg[chosenPetsScript.petsChosen.Count].GetComponent<Button>().enabled = false;
+
             UpdateChosenPets();
+            
+            uiObj[0].SetActive(false);//the whole panel
+            uiObj[1].SetActive(true);//Add button
+            uiObj[2].SetActive(false);//Remove button
         }
     }
 
@@ -59,12 +72,19 @@ public class PetStatsText : MonoBehaviour
 
             petInfoText[1].text = "Allergens: " + allergensText;
             petInfoText[2].text = "Symptoms: " + symptomsText;
-            petInfoText[3].text = "Personality: " + statsRandomizer.personality;
-            petInfoText[4].text = "Curiosity: " + statsRandomizer.curiosity;
-            petInfoText[5].text = "Attention Span: " + statsRandomizer.attentionSpan;
-            petInfoText[6].text = "Thirst: " + statsRandomizer.thirst;
-            petInfoText[7].text = "Hunger: " + statsRandomizer.hunger;
-            petInfoText[8].text = "Love: " + statsRandomizer.love;
+            petInfoText[3].text = "Personality: ";
+            statSliders[0].value = statsRandomizer.personality;
+            petInfoText[4].text = "Curiosity: ";
+            statSliders[1].value = statsRandomizer.curiosity;
+            petInfoText[5].text = "Attention Span: ";
+            statSliders[2].value = statsRandomizer.attentionSpan;
+            petInfoText[6].text = "Thirst: ";
+            statSliders[3].value = statsRandomizer.thirst;
+            petInfoText[7].text = "Hunger: ";
+            statSliders[4].value = statsRandomizer.hunger;
+            petInfoText[8].text = "Love: ";
+            statSliders[5].value = statsRandomizer.love;
+
         }
         else
         {
@@ -87,12 +107,23 @@ public class PetStatsText : MonoBehaviour
 
             petInfoText[1].text = "Allergens: " + allergensText;
             petInfoText[2].text = "Symptoms: " + symptomsText;
-            petInfoText[3].text = "Personality: " + chosenPetsScript.petsChosen[whichPet].personality;
-            petInfoText[4].text = "Curiosity: " + chosenPetsScript.petsChosen[whichPet].curiosity;
-            petInfoText[5].text = "Attention Span: " + chosenPetsScript.petsChosen[whichPet].attentionSpan;
-            petInfoText[6].text = "Thirst: " + chosenPetsScript.petsChosen[whichPet].thirst;
-            petInfoText[7].text = "Hunger: " + chosenPetsScript.petsChosen[whichPet].hunger;
-            petInfoText[8].text = "Love: " + chosenPetsScript.petsChosen[whichPet].love;
+            petInfoText[3].text = "Personality: ";
+            statSliders[0].value = chosenPetsScript.petsChosen[whichPet].personality;
+            petInfoText[4].text = "Curiosity: ";
+            statSliders[1].value = chosenPetsScript.petsChosen[whichPet].curiosity;
+            petInfoText[5].text = "Attention Span: ";
+            statSliders[2].value = chosenPetsScript.petsChosen[whichPet].attentionSpan;
+            petInfoText[6].text = "Thirst: ";
+            statSliders[3].value = chosenPetsScript.petsChosen[whichPet].thirst;
+            petInfoText[7].text = "Hunger: ";
+            statSliders[4].value = chosenPetsScript.petsChosen[whichPet].hunger;
+            petInfoText[8].text = "Love: ";
+            statSliders[5].value = chosenPetsScript.petsChosen[whichPet].love;
+            
+            //Setting up UI
+            uiObj[0].SetActive(true);
+            uiObj[1].SetActive(false);
+            uiObj[2].SetActive(true);
         }
     }
 }
