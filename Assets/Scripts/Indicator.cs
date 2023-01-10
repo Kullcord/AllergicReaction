04b2aba0791e4a859c518492b6d3809381e32965
Kullcord <58976043+Kullcord@users.noreply.github.com>
2023-01-10 07:52:53 +0100

    Added Indicator script
    Used for changing icons

--- Assets/Scripts/Indicator.cs
+++ Assets/Scripts/Indicator.cs
@@ -1,39 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Indicator : MonoBehaviour
 {
-    public int indicatorID;
-    [SerializeField] private StateManager savedPet;
-    [SerializeField] private CameraHandler camHolder;
-
-    //List of all pet's in the scene
-    [SerializeField] private List<StateManager> pets;
-
-
-    private void Start()
-    {
-        pets = new List<StateManager>(FindObjectsOfType<StateManager>());
-
-        foreach(StateManager pet in pets)
-        {
-            int petID = pet.id;
-
-            if(petID == indicatorID)
-            {
-                savedPet = pet;
-                break;
-            }
-        }
-    }
-
-    public void TeleportToPet()
-    {
-        var newX = savedPet.gameObject.transform.position.x - camHolder.offsetX;
-        var newZ = savedPet.gameObject.transform.position.z + camHolder.offsetZ;
-        var newPos = new Vector3(newX, camHolder.transform.position.y, newZ);
-
-        camHolder.cam.transform.position = newPos;
-    }
+    public Button petIcon;
+    public RawImage actionIcon;
+    public int id;
 }
